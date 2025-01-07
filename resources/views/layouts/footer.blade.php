9994f0515c5e1153530fa45b482d3708265f7d2a
boka matyas <boka.matyas@students.jedlik.eu>
2025-01-07 11:23:45 +0100

    Adding layouts folder

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="container-fluid text-center bg-dark text-white py-3">
+    &copy; 2024 BG
+</div>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,3 @@
+<div class="container-fluid p-0">
+    <img src="{{ asset('images/laravel-controllers.jpg') }}" alt="" class="w-100">
+</div>
